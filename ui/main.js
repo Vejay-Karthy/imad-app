9906959eea96dbb0a86f09be1e7ab21dc8ce8a76
Vejay-Karthy <36543956+Vejay-Karthy@users.noreply.github.com>
2018-02-19 20:04:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,8 +24,22 @@ img.onclick = function() {
 var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function() {
-  //Render the variable in the correct span
-  counter = counter + 1;
-  var span = document.getElementById('count');
-  span.innerHTML = counter.toString();
+    //Create a request
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function() {
+            if(request.readyState === XMLHttpRequest.DONE){
+                //Take some action
+                if(request.status === 200) {
+                    var counter = request.responseText;
+                    var span = document.getElementById('count');
+                    span.innerHTML = counter.toString();
+                    
+                }
+            }
+    }
+//Make the request
+request.open('GET', 'http://http://svejay1999.imad.hasura-app.io/counter', true);
+request.send(null);
+  
 };
