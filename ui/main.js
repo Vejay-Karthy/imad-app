7e371b50d8667350d715f27921c478e572c64ab3
Vejay-Karthy <36543956+Vejay-Karthy@users.noreply.github.com>
2018-02-19 20:19:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,26 +1,5 @@
 console.log('Loaded!');
 
-
-
-/*var element = document.getElementById('main-text');
-
-
-element.innerHTML = 'New Value';
-
-
-
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveright(){
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-    
-}
-img.onclick = function() {
-    var interval = setInterval(moveRight,50);
-};
-*/
-    
 var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function() {
